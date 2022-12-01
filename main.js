1c2a94ba22a1f052d1c4eb12260b71b810bdf34a
IdaBjoern <ida.b.bjoern@gmail.com>
2022-12-01 13:54:21 +0100

    Lagt queries ind, tilføjet barchart, samt css fil

--- main.js
+++ main.js
@@ -56,12 +56,11 @@ client.connect();
 
 app.use(express.text());
 app.use(express.static("public"))
-app.use(morgan("combined")); 
+app.use(morgan("combined"));
 
 /*
  * Her defineres API'en.
- * Man laver lige så mange endpoints man har lyst til. Jeg har lavet et enkelt til
- * querien `SELECT 'Hello, World' as message`.
+ * API: Gennemsnitspris per time (2021) til landing-page
  */
 app.post("/api/diagram1", async (req, res) => {
   try {
@@ -82,6 +81,107 @@ app.post("/api/diagram1", async (req, res) => {
     })
   }
 });
+
+
+
+// El-afgifter 
+app.post("/api/elafgift", async (req, res) => {
+  try {
+    // Lav query
+    const query = `SELECT * FROM elafgift`;
+    queryData = await client.query(query);
+    // Giv svar tilbage til JavaScript
+    res.json({
+      "ok": true,
+      "data": queryData.rows,
+    })
+  } catch (error) {
+    // Hvis query fejler, fanges det her.
+    // Send fejlbesked tilbage til JavaScript
+    res.json({
+      "ok": false,
+      "message": error.message,
+    })
+  }
+});
+
+
+
+
+
+
+// Transmission, system-tarif og balancetarif for 2021 (sammenlagt)
+app.post("/api/tarif_sum", async (req, res) => {
+  try {
+    // Lav query
+    const query = `SELECT SUM(COALESCE(transmission,0) + COALESCE(system_tarif,0) + COALESCE(balance_tarif,0)) FROM tariffer WHERE periode = '2021-01-01'`;
+    queryData = await client.query(query);
+    // Giv svar tilbage til JavaScript
+    res.json({
+      "ok": true,
+      "data": queryData.rows,
+    })
+  } catch (error) {
+    // Hvis query fejler, fanges det her.
+    // Send fejlbesked tilbage til JavaScript
+    res.json({
+      "ok": false,
+      "message": error.message,
+    })
+  }
+});
+
+
+
+
+
+
+// Ugentlige gennemsnints max- og minpris 
+app.post("/api/avg_weekly", async (req, res) => {
+  try {
+    // Lav query
+    const query = `SELECT AVG(maximum) AS max_avg, AVG(minimum) AS min_avg, week_nr FROM max_min_pris GROUP BY week_nr ORDER BY week_nr`;
+    queryData = await client.query(query);
+    // Giv svar tilbage til JavaScript
+    res.json({
+      "ok": true,
+      "data": queryData.rows,
+    })
+  } catch (error) {
+    // Hvis query fejler, fanges det her.
+    // Send fejlbesked tilbage til JavaScript
+    res.json({
+      "ok": false,
+      "message": error.message,
+    })
+  }
+});
+
+// Hourly gennemsnitspris (2021)
+app.post("/api/avg_timepris", async (req, res) => {
+  try {
+    // Lav query
+    const query = `SELECT AVG ("spotpris_dkk_mwh"), EXTRACT(HOUR FROM tidspunkt_dk) AS HOUR FROM spotpris GROUP BY hour ORDER BY hour`;
+    queryData = await client.query(query);
+    // Giv svar tilbage til JavaScript
+    res.json({
+      "ok": true,
+      "data": queryData.rows,
+    })
+  } catch (error) {
+    // Hvis query fejler, fanges det her.
+    // Send fejlbesked tilbage til JavaScript
+    res.json({
+      "ok": false,
+      "message": error.message,
+    })
+  }
+});
+
+
+
+
+
 
 app.get("/api/hello", async (req, res) => {
   res.json({ "message": "Hello, World!" });
